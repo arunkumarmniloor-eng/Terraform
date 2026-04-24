6b387cceba592fa3e52271c8153eacdbbae72c69
arunkumarmniloor-eng <arunkumarmniloor@gmail.com>
2026-04-24 17:40:07 +0000

    updated

--- flask/app.py
+++ flask/app.py
@@ -1,20 +1,20 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
+@app.route("/")
 def home():
-        return jsonify({
-                    "message": "Flask backend is running 🚀"
-                        })
+    return jsonify({"message": "Flask backend is running 🚀"})
 
-        @app.route('/api/data')
-        def data():
-                return jsonify({
-                            "name": "Arun",
-                                    "role": "DevOps Engineer",
-                                            "project": "Flask + Express Deployment"
-                                                })
+@app.route("/api/data")
+def data():
+    return jsonify({
+        "name": "Arun",
+        "role": "DevOps Engineer",
+        "project": "Flask + Express Deployment"
+    })
 
-                if __name__ == '__main__':
-                        app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
